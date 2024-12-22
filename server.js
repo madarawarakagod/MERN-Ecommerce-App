3a2d9c93c8ed38522f1fe1bf8ffe160261c7a59f
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-12-22 13:24:43 +0530

    nodemon

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require("express");
-
+import express from "express";
+import colors from "colors";
 //rest object
 const app = express();
 
 //rest api
 app.get("/", (req, res) => {
-    res.send({
-    message: "Welcome to ecommer app",
+    res.send(
+        "<h1>Welcome to ecommerce app</h1>");
   });
-});
 
 //PORT
 const PORT = 8080;
@@ -16,7 +15,8 @@ const PORT = 8080;
 //run listen
 app.listen(PORT, () => {
     console.log(
-      `Server Running on ${PORT}`);
+      `Server Running on ${PORT}`.bgCyan
+      .white);
         
     
   });
