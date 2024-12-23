f49d2e6bb11b08297dcb456269a5d6c3d2a2b664
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-12-23 10:25:19 +0530

    changes routes,midlewares

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import express from "express";
 import colors from "colors";
+import dotenv from "dotenv";
+import morgan from  "morgan";
+import connectDB from "./config/db.js";
+
+//configure env
+dotenv.config();
+
+//databse config
+connectDB();
+
 //rest object
 const app = express();
+
+//middelwares
+app.use(express.json());
+app.use(morgan("dev"));
+
 
 //rest api
 app.get("/", (req, res) => {
@@ -10,14 +25,14 @@ app.get("/", (req, res) => {
   });
 
 //PORT
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
+//run listen
 //run listen
 app.listen(PORT, () => {
     console.log(
-      `Server Running on ${PORT}`.bgCyan
-      .white);
-        
-    
+      `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+        .white
+    );
   });
   
