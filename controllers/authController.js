3f92d6c424b4ec24c77681068759221c3411f956
madarawarakagoda <madarawarakagoda8@gmail.com>
2024-12-23 23:20:52 +0530

    change controllers and routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import userModel from "../models/userModel.js";
-import { hashPassword } from "./../helpers/authHelper.js";
+import {comparePassword,  hashPassword } from "./../helpers/authHelper.js";
+import JWT from "jsonwebtoken";
 
 export const registerController = async (req, res) => {
   try {
@@ -9,16 +10,16 @@ export const registerController = async (req, res) => {
       return res.send({ error: "Name is Required" });
     }
     if (!email) {
-      return res.send({ message: "Email is Required" });
+      return res.send({ error: "Email is Required" });
     }
     if (!password) {
-      return res.send({ message: "Password is Required" });
+      return res.send({ error: "Password is Required" });
     }
     if (!phone) {
-      return res.send({ message: "Phone no is Required" });
+      return res.send({ error: "Phone no is Required" });
     }
     if (!address) {
-      return res.send({ message: "Address is Required" });
+      return res.send({ error: "Address is Required" });
     }
     
     //check user
@@ -52,6 +53,59 @@ export const registerController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Errro in Registeration",
+      error,
+    });
+  }
+};
+ 
+//POST LOGIN
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    //check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({       
+        success: false,
+        message: "Email is not registerd",
+      });
+    }
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+    //token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "login successfully",
+      user: {
+       
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+        
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in login",
       error,
     });
   }
--- models/userModel.js
+++ models/userModel.js
@@ -24,10 +24,7 @@ const userSchema = new mongoose.Schema(
       type: {},
       required: true,
     },
-    answer: {
-      type: String,
-      required: true,
-    },
+    
     role: {
       type: Number,
       default: 0,
