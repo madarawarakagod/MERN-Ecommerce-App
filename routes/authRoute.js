eeca4be5674f870c3db454dfb9e008d900ed92ee
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-01-31 23:36:16 +0530

    auth

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {registerController,loginController,testController,forgotPasswordController} from "../controllers/authController.js";
 
-import {isAdmin,requireSignIn} from "../middlewares/authMiddlewares.js";
+import {isAdmin,requireSignIn} from "../middlewares/authMiddleware.js";
 
 //router object
 const router = express.Router();
