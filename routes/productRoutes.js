e005bb1f8a556099536db2e5196ab9b50c0bf60b
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-02-02 14:20:37 +0530

    create product and some packages install

---
+++ routes/productRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {createProductController} from "../controllers/productController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+//routes
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+ formidable(),
+  createProductController
+);
+
+
+export default router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import morgan from  "morgan";
 import connectDB from "./config/db.js";
 import authRoutes  from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js"
+import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors';
 //configure env
 dotenv.config();
