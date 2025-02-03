e07462edee795d6833755c099e8d81ce78d2eca4
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-02-03 12:29:13 +0530

    create category

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import Layout from '../../components/Layout/Layout';
 import AdminMenu from './../../components/Layout//AdminMenu';
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const CreateCategory = () => {
+  const [categories,setCategories]=useState([]);
+
+  //handle Form
+  const getAllCategory = async () => {
+ 
+    try {
+      const { data } = await axios.post("/api/v1/category/create-category")
+      if (data?.success) {
+        setCategories(data.category);
+      } 
+
+    } catch (error) {
+      console.log(error);
+      toast.error("somthing went wrong in input form");
+    }
+  };
+
+  useEffect(()=>{
+    getAllCategory();
+  },[]);
+
   return (
      
               <Layout title={"Dashboard - Create Category"}>
@@ -12,15 +35,35 @@ const CreateCategory = () => {
                   <AdminMenu />
                 </div>
                 <div className="col-md-9">
-                    <h1>Create Category</h1>
-                  
+                    <h1>Manage Category</h1>
+               <div>     
+               <table className='table'>   
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>  
+                  {categories?.map((c) => (
+                    <td key={c._id}>{c.name}</td>
+                  ))}
+                  </tr>
+                  </tbody>
+                  </table>
+                </div>
+                </div>
                 </div>
               </div>
-              </div>
-          </Layout>
-        );
-      };
+                </Layout>
+               );
+              };
+ export default CreateCategory;
+                       
+
+
+
   
 
 
-export default CreateCategory
