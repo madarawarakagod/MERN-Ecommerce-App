54fee49698ca3e84f869efbb64b670c0c626278a
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-02-10 00:14:47 +0530

    cartpage,ordermodel

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -5,6 +5,7 @@ import { useAuth } from "../context/auth";
 import { useNavigate } from "react-router-dom";
 import DropIn from "braintree-web-drop-in-react";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const CartPage = () => {
   const [auth, setAuth] = useAuth();
@@ -55,6 +56,27 @@ const CartPage = () => {
   useEffect(() => {
     getToken();
   }, [auth?.token]);
+
+    //handle payments
+    const handlePayment = async () => {
+      try {
+        setLoading(true);
+        const { nonce } = await instance.requestPaymentMethod();
+        const { data } = await axios.post("/api/v1/product/braintree/payment", {
+          nonce,
+          cart,
+        });
+        setLoading(false);
+        localStorage.removeItem("cart");
+        setCart([]);
+        navigate("/dashboard/user/orders");
+        toast.success("Payment Completed Successfully ");
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
+
 
   return (
     <Layout>
@@ -144,6 +166,25 @@ const CartPage = () => {
                     )}
                 </div>
              )}
+             <div className="mt-2">
+             
+                   <DropIn
+                     options={{
+                       authorization: clientToken,
+                       paypal: {
+                         flow: "vault",
+                       },
+                     }}
+                     onInstance={(instance) => setInstance(instance)}
+                   />
+                  <button
+                      className="btn btn-primary"
+                      onClick={handlePayment}
+                      disabled={!loading || !instance|| !auth?.user?.address}
+                    >
+                     {loading ? "Processing ...." : "Make Payment"}
+                    </button>
+                    </div>
         </div>
         </div>
       </div>
